3896acf2a8a9327e15149fb5240867f0367660e2
conceptualio <76885149+conceptualio@users.noreply.github.com>
2021-01-03 12:07:51 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
   name = 'yacache',
   packages = ['yacache'],
-  version = '0.1.3',
+  version = '0.1.4',
   license = 'MIT',
   description = 'yacache is yet another python cache. This library allows for caching function results in whatever type they were initially returned. This is meant to be fast, optional, and, most importantly, retain all docstrings unlike some other caching libs.',
   author = 'conceptual.io',
   author_email = 'admin@conceptual.io',
   url = 'https://github.com/conceptualio',
-  download_url = 'https://github.com/conceptualio/yacache/archive/0.1.3.tar.gz',
+  download_url = 'https://github.com/conceptualio/yacache/archive/0.1.4.tar.gz',
   keywords = ['CACHE'],
   install_requires = [
           'datetime',
